3b22de7d9ffba7988adcdba12db0869c7ffc8192
Ananya Teklu <teklu009@morris.umn.edu>
2023-10-21 23:00:52 -0500

    Finialize homepage design

--- src/components/header-and-footer/footer.tsx
+++ src/components/header-and-footer/footer.tsx
@@ -6,26 +6,26 @@ const Footer = () => {
   return (
     <div className="footer">
       <img src={Logo} alt='logo' className="footerLogo" />
-      <div className="footer-links">
+     
+      <div className="footer-contact">
+        <p>Email: support@karamara.com</p>
+        <p>Phone: +1 (612) 743-7243</p>
+      </div>
+      {/* <div className="footer-social">
+        <img src="path_to_facebook_icon.png" alt="Facebook" />
+        <img src="path_to_twitter_icon.png" alt="Twitter" />
+        <img src="path_to_instagram_icon.png" alt="Instagram" />
+      </div> */}
+      <div className="footer-copyright">
+   <div className="footer-links">
         <ul>
+          <li>© 2023 Karamara. All rights reserved.</li>
           <li>About Us</li>
           <li>Contact</li>
           <li>Privacy Policy</li>
           <li>Terms of Service</li>
         </ul>
       </div>
-      <div className="footer-contact">
-        <p>Email: support@karamara.com</p>
-        <p>Phone: +123-456-7890</p>
-      </div>
-      <div className="footer-social">
-        {/* Replace these with your own social media icons */}
-        <img src="path_to_facebook_icon.png" alt="Facebook" />
-        <img src="path_to_twitter_icon.png" alt="Twitter" />
-        <img src="path_to_instagram_icon.png" alt="Instagram" />
-      </div>
-      <div className="footer-copyright">
-        <p>© 2023 Karamara. All rights reserved.</p>
       </div>
     </div>
   );
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,15 +5,19 @@ import Handbag2 from "../../assets/handbag2.png";
 import Shoes1 from "../../assets/mensshoes1.png";
 import Shoes2 from "../../assets/mensshoes2.png";
 import Shoes3 from "../../assets/womansshoes1.png";
+import Mens1 from "../../assets/mens1.png";
+import Womans1 from "../../assets/womans1.png";
+import Floatbag from "../../assets/womansb1.png";
+import Tie1 from "../../assets/tie1.png";
 
 // MainBanner.js
 const MainBanner = () => {
   return (
     <div className="main-banner">
-      <h2>NEW ARRIVAL!</h2>
+
       <h3>ITALIAN WOMAN'S BAG</h3>
       <p>Stylish woman's bag hand made with real leather in Italy</p>
-      <button>View Product</button>
+      <button>View Products</button>
     </div>
   );
 };
@@ -22,7 +26,7 @@ const SecondaryBanner = () => {
   return (
     <div className="secondary-banner">
       {/* You can place content or additional images here */}
-      <h4>Featured Item</h4>
+      <h3>MEN'S SHOES</h3>
       <p>Discover our latest collection...</p>
     </div>
   );
@@ -31,7 +35,7 @@ const SecondaryBanner = () => {
 
 // DealsSection.js
 const DealsSection = () => {
-  const exampleProducts = [
+  const exampleBestDeals = [
     { id: 1, name: 'Nike Downshifter 12', price: '$ 60', image: Handbag1 },
     { id: 2, name: 'Compass Retrograde Hi...', price: '$ 80', image: Handbag2 },
     { id: 3, name: 'Adidas Superstar X LG...', price: '$ 120', image: Shoes1 },
@@ -43,7 +47,7 @@ const DealsSection = () => {
     <div className="deals-section">
       <h3>Today Best Deals!</h3>
       <div className="deals-grid">
-        {exampleProducts.map(product => (
+        {exampleBestDeals.map(product => (
           <div key={product.id} className="deal-item">
             <img src={product.image} alt={product.name} />
             <p className="item-name">{product.name}</p>
@@ -55,11 +59,24 @@ const DealsSection = () => {
   );
 };
 // BrandsSection.js
-const BrandsSection = () => {
+const ProductsSection = () => {
+  const exampleProducts = [
+    { id: 1, name: "Men's Apparel", image: Mens1 },
+    { id: 2, name: "Women's Apparel", image: Womans1 },
+    { id: 3, name: "Women's Bag", image: Floatbag },
+    { id: 4, name: "Men's Tie", image: Tie1 },
+    //... add more example products
+  ];
   return (
-    <div className="brands-section">
-      <h3>Some of The Amazing Brand</h3>
-      {/* Map over your brands array and render each brand */}
+    <div className="products-section">
+      <div className="products-grid">
+        {exampleProducts.map(product => (
+          <div key={product.id} className="product-item">
+            <img src={product.image} alt={product.name} />
+            <p className="product-name">{product.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -73,7 +90,7 @@ const HomePage = () => {
         <SecondaryBanner />
       </div>
       <DealsSection />
-      <BrandsSection />
+      <ProductsSection />
     </div>
   );
 };
